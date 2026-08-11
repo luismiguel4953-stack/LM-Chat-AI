bdc85b18410c1392f1776bf26b1feca7cbad40ab
CodeByLuis <luismiguel4953@gmail.com>
2026-08-11 12:51:35 +0100

    feat: add offline service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "lm-chat-ai-v3";
+const ASSETS = ["./", "./index.html", "./css/style.css", "./js/app.js", "./manifest.webmanifest"];
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
